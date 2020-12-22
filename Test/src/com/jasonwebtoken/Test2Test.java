cdc35f3d5946b56c074c822a46316841eeca0b3c
Óscar <scar@DESKTOP-9TS7BBP>
2020-12-21 20:21:42 -0500

    Upload 2 - Multiple Tabs

--- Test/src/com/jasonwebtoken/Test2Test.java
+++ Test/src/com/jasonwebtoken/Test2Test.java
@@ -6,7 +6,9 @@ import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -24,10 +26,10 @@ public class Test2Test {
   public static void main(String args[]) {
 	  
 	  
-      //  System.setProperty("webdriver.chrome.driver", "C:\\Users\\oscar\\OneDrive\\Documents\\WebDrivers\\Chrome\\chromedriver.exe");
-      	  System.setProperty("webdriver.gecko.driver","C:\\Users\\oscar\\OneDrive\\Documents\\WebDrivers\\FireFox\\geckodriver.exe");
-	  
-	  JUnitCore junit = new JUnitCore();
+      System.setProperty("webdriver.chrome.driver", "C:\\Users\\oscar\\OneDrive\\Documents\\WebDrivers\\Chrome\\chromedriver.exe");
+      //System.setProperty("webdriver.gecko.driver","C:\\Users\\oscar\\OneDrive\\Documents\\WebDrivers\\FireFox\\geckodriver.exe");
+      	  
+      JUnitCore junit = new JUnitCore();
 	  junit.addListener(new TextListener(System.out));
 	  Result result = junit.run(Test2Test.class); 
 	  if (result.getFailureCount() > 0) {
@@ -42,30 +44,53 @@ public class Test2Test {
   
   @Before
   public void setUp() {
-    //driver = new ChromeDriver();
-	driver = new FirefoxDriver();
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments(Arrays.asList("disable-infobars", "ignore-certificate-errors", "start-maximized","use-fake-ui-for-media-stream"));
+	driver = new ChromeDriver(options);
+    //driver = new FirefoxDriver();
     js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
+    vars = new HashMap<String, Object>();    
   }
   @After
   public void tearDown() {
-    driver.quit();
+    //driver.quit();
   }
+  
   @Test
-  public void test2() throws InterruptedException {
+  public void main() throws InterruptedException {
+	  int k=0;	  
+	  for (int i = 143; i < 146; ++i) {
+		  test2(i,k);
+		  k++;
+		}
+  }
+  
+  
+  
+  public void test2(int iter, int wind) throws InterruptedException {
 	  
-	//Open web site 1 
-    driver.get("https://www.jsonwebtoken.io/");
-    driver.manage().window().setSize(new Dimension(974, 1040));
+	driver.manage().window().maximize();  
+	//Open web site 1
+	if (iter != 143) {
+		((JavascriptExecutor)driver).executeScript("window.open()");
+
+		    ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+		    driver.switchTo().window(tabs.get(wind)); //switches to new tab
+
+	}
+		
+	driver.navigate().to("https://www.jsonwebtoken.io/");
+    //driver.get("https://www.jsonwebtoken.io/");
+   
     //Clear and write secret field
     driver.findElement(By.name("jwtKey")).clear();
     driver.findElement(By.name("jwtKey")).sendKeys("tlcN1D66yOs61shkfxWoVKCXC3qPgqzzx-nt_MhLUQETsZmrpcNwFh0HELxbcP7Y");
-    Thread.sleep(5000);
+    Thread.sleep(1000);
     //Select and overwrite payload field 
     driver.findElement(By.cssSelector(".col-sm-8 > .panel")).click();
     driver.findElement(By.cssSelector(".CodeMirror-focused textarea")).sendKeys(Keys.CONTROL + "a");
     driver.findElement(By.cssSelector(".CodeMirror-focused textarea")).sendKeys("{\n" + 
-    		" \"sub\": \"collage|143\",\n" + 
+    		" \"sub\": \"collage|"+iter+"\",\n" + 
     		" \"name\": \"Johna Doe\",\n" + 
     		" \"email\": \"jarv078@gmail.com\",\n" + 
     		" \"admin\": true,\n" + 
@@ -78,9 +103,11 @@ public class Test2Test {
     String element = driver.findElement(By.id("jwtInput")).getAttribute("value");
     System.out.print(element);
     //Go to web site 2
-    driver.get("https://live-staging.verb.tech/login/auth0?token=" + element);
-    Thread.sleep(10000);
+    driver.navigate().to("https://live-staging.verb.tech/login/auth0?token=" + element);
+    Thread.sleep(1000);
   }
+  
+ 
   
   
 }
